59db3abb9f1516763868bb18cb6ee5b2247a139c
vicentemorales <vmorales121@gmail.com>
2023-06-04 13:27:40 -0700

    test app js code

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react"
+import "./App.css"
+import { useState } from "react"
+
+function App() {
+  const [btnColor, setBtnColor] = useState("green")
+  const newBtnColor = btnColor === "green" ? "blue" : "green"
+  const [inputDisabled, setInputDisabled] = useState(false)
+
+  return (
+    <div className="App">
+      <h1>Testing Playground</h1>
+      <button
+        style={{ backgroundColor: btnColor }}
+        onClick={() => setBtnColor(newBtnColor)}
+        disabled={inputDisabled}
+      >
+        Change to {newBtnColor}
+      </button>
+
+      <input
+        type="checkbox"
+        defaultChecked={inputDisabled}
+        onChange={(event) => setInputDisabled(event.target.checked)}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/App.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react"
+import App from './App'
+ import userEvent from '@testing-library/user-event' 
+
+test("header renders with correct text", () => {
+  render(<App />)
+  const headerEl = screen.getByRole("heading")
+  expect(headerEl.textContent).toBe("Testing Playground")
+})
+
+describe('test for the button', () =>{
+  test("button changes color when clicked", () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+  
+    userEvent.click(colorBtn)
+  
+    expect(colorBtn).toHaveStyle({ backgroundColor: "blue" })
+    expect(colorBtn.textContent).toBe("Change button color to green")
+  })
+}) 
+
+
